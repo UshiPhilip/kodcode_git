6f9d0b5db7f2ec3bfae88404563d56ce2b560ec7
Ushi Philip <ushi7670741@gmail.com>
2026-05-13 16:25:00 +0300

    Fixed validations in functions

--- week5/python_basic/clean_code/dont_repeat_yourself.py
+++ week5/python_basic/clean_code/dont_repeat_yourself.py
@@ -1,6 +1,12 @@
 def create_admin_user(name, email, role):
-    if not name or len(name) < 2:
+    if not valid_name:
         raise ValueError("Invalid name")
-    if "@" not in email:
+    if not valid_email:
         raise ValueError("Invalid email")
     return name, email, role, "2024-01-01", True
+
+def valid_name(name):
+    return bool(name) and len(name) >= 2
+
+def valid_email(email):
+    return "@" in email
