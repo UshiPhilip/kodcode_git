905d4815e7928d4360d7a7c6521bd72557f34343
Ushi Philip <ushi7670741@gmail.com>
2026-05-14 12:31:08 +0300

    Orgenized the code used Black Formatter extantion

--- week5/python_basic/tasks_manager/main.py
+++ week5/python_basic/tasks_manager/main.py
@@ -19,13 +19,14 @@ class Task:
     Priority level: {"High" if self.priority_level == 3 else "Medium" if self.priority_level == 2 else "Low"}
     Status: {"Completed" if self.is_complete else "Not completed"}\n\n
 """
-        
-        
+
+
 def print_task_stats(task_name, tasks):
     for task in tasks:
         if task.name == task_name:
             return task
     return f"Task {task_name} not found..."
+
 
 def mark_as_completed(task_name, tasks):
     for task in tasks:
@@ -34,21 +35,26 @@ def mark_as_completed(task_name, tasks):
             return f"Task: {task_name} marked as complete\nGreat job!"
     return f"Task {task_name} not found..."
 
+
 def mark_as_incomplete(task_name, tasks):
     for task in tasks:
         if task.name == task_name:
             task.mark_incomplete()
             return f"Task: {task_name} marked as incomplete\nHurry up..."
-    return f"Task {task_name} not found..."  
+    return f"Task {task_name} not found..."
+
 
 def incompleted_tesks(tasks):
-    return [task for task in tasks if not task.is_complete] 
+    return [task for task in tasks if not task.is_complete]
+
 
 def completed_tasks(tasks):
     return [task for task in tasks if task.is_complete]
 
+
 def identify_urgent_tasks(tasks):
     return [task for task in tasks if task.priority_level == 3]
+
 
 def delete_task(task_name, tasks):
     for task in tasks:
@@ -56,14 +62,16 @@ def delete_task(task_name, tasks):
             return task
     return False
 
+
 def get_daily_summary(tasks):
     number_of_incomplete = len(incompleted_tesks(tasks))
     number_of_completed = len(completed_tasks(tasks))
     number_of_urgent_tasks = len(identify_urgent_tasks(tasks))
     return [number_of_incomplete, number_of_completed, number_of_urgent_tasks]
 
+
 def menu():
-    return"""
+    return """
     ========= TASKS MANAGER =========
 
     1. Add a task to the task list
@@ -77,6 +85,7 @@ def menu():
     9. Delete task
 """
 
+
 def main():
     tasks = []
     print("=======================================================")
@@ -86,31 +95,31 @@ def main():
         print(tasks)
         print(menu())
         user_choice = input("Enter your choice [or quit to exit]: ")
-        
+
         if user_choice == "quit":
             print("Bye bye\nSee you next time...")
             exit()
-        
+
         if user_choice == "1":
             task_name = input("Enter your task name: ")
             priority_level = int(input("Enter the priority task's level [1-3]: "))
             tasks.append(Task(task_name, priority_level))
             print(f"Task {task_name} added successully to your tasks list!")
-        
+
         elif user_choice == "2":
             task_name = input("Enter a task name are you looking for: ")
             print("=================================")
             print(print_task_stats(task_name, tasks))
             print("=================================")
-        
+
         elif user_choice == "3":
             task_name = input("Which task did you complete? ")
             print(mark_as_completed(task_name, tasks))
-        
+
         elif user_choice == "4":
             task_name = input("Which task do you want to mark as incomplete? ")
             print(mark_as_incomplete(task_name, tasks))
-        
+
         elif user_choice == "5":
             all_completed = completed_tasks(tasks)
             if not all_completed:
@@ -119,7 +128,7 @@ def main():
             print("All completed tasks:")
             for task in all_completed:
                 print(task, end="|")
-        
+
         elif user_choice == "6":
             all_incomplete = incompleted_tesks(tasks)
             if not all_incomplete:
@@ -129,7 +138,7 @@ def main():
             for task in all_incomplete:
                 print(task, end="|")
             print("\nHurry up to compelte them!")
-        
+
         elif user_choice == "7":
             urgent_tasks = identify_urgent_tasks(tasks)
             print("All urgent tasks:")
@@ -139,7 +148,7 @@ def main():
             for task in urgent_tasks:
                 print(task, end="|")
             print("\nIn case you didn't complete them, you know what to do...")
-        
+
         elif user_choice == "8":
             daily_summary = get_daily_summary(tasks)
             print(f"""
